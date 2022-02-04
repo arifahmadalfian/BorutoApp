682436f5bde6599ac24f538cf2818b2568ec505c
arifahmadalfian <arifahmadalfian@gmail.com>
2022-02-04 21:48:58 +0700

    init

--- app/src/main/java/com/arifahmadalfian/borutoapp/MainActivity.kt
+++ app/src/main/java/com/arifahmadalfian/borutoapp/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.arifahmadalfian.borutoapp.navigation.SetupNavGraph
 import com.arifahmadalfian.borutoapp.ui.theme.BorutoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
---
+++ app/src/main/java/com/arifahmadalfian/borutoapp/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.arifahmadalfian.borutoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application()
